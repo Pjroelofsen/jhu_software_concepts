b6061e52c9d7bccfc3df19e10d34396ba36c61a1
Pjroelofsen <160357984+Pjroelofsen@users.noreply.github.com>
2025-05-23 09:53:55 -0400

    Initial Commit
    initial structure of website with my information filled in

---
+++ module_1/app/main/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from . import main
+
+@main.route('/')
+def home():
+    return render_template('index.html', active='home')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html', active='contact')
+
+@main.route('/projects')
+def projects():
+    return render_template('projects.html', active='projects')
